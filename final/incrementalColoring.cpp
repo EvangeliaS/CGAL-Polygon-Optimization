264dafcd8eb9a8fee6195e71852a5ae7a00bdd71
master <stathislo13@gmail.com>
2022-11-02 22:14:56 +0200

    Before inverting edges

--- final/incrementalColoring.cpp
+++ final/incrementalColoring.cpp
@@ -59,11 +59,24 @@ std::vector<std::string> split(const std::string& s, char delimiter)
    return tokens;
 }
 
+class EdgeAreaTuple {
+public:
+    Polygon_2::Segment_2 edge;
+    double area;
+
+    EdgeAreaTuple(Polygon_2::Segment_2 edge, double area)
+     : edge(edge), area(area)
+    {}
+};
+
 
 int findVertex(std::vector<ColoredVertex> coloredVertices, Point_2 point);
 int findPolygonPoint(Polygon_2 polygon, Point_2 point);
 void vertexColoring(std::vector<ColoredVertex>& coloredVertices, int color, Point_2 point);
-void findVisibleEdges(Polygon_2 polygon, Point_2 point, ColoredEdge redEdge, std::vector<Polygon_2::Segment_2>& visibleEdges);
+void findVisibleEdges(Polygon_2 polygon, Point_2 point, ColoredEdge redEdge, std::vector<EdgeAreaTuple>& visibleEdges);
+int findMinAreaEdge(std::vector<EdgeAreaTuple> visibleEdges);
+int findMaxAreaEdge(std::vector<EdgeAreaTuple> visibleEdges);
+
 
 
 
@@ -75,7 +88,7 @@ int main(int argc, char** argv)
     //     return ERROR;
     // }
     std::string inputFile = argv[2];
-    inputFile ="../instances/data/images/euro-night-0000010.instance";
+    inputFile ="../instances/data/images/us-night-0000100.instance";
 
     std::ifstream input(inputFile);
     if (!input) {
@@ -190,7 +203,7 @@ int main(int argc, char** argv)
             std::cout << coloredEdges[j].edge << ", color: " << coloredEdges[j].color << std::endl;
         }
 
-        std::vector<Polygon_2::Segment_2> visibleEdges;
+        std::vector<EdgeAreaTuple> visibleEdges;
 
         // For every red edge
         for (int j = 0; j < coloredEdges.size(); j++)
@@ -246,14 +259,28 @@ int main(int argc, char** argv)
         std::cout << "Visible Edges:" << std::endl;
         for (int k = 0; k < visibleEdges.size(); k++)
         {
-            std::cout << visibleEdges[k] << std::endl;
+            std::cout << visibleEdges[k].edge << "  -- Area: " << visibleEdges[k].area << std::endl;
         }
-        Polygon_2::Segment_2 edge = visibleEdges[0];
+
+        // Chose the visible edge
+        int index = -1;
+        index = findMinAreaEdge(visibleEdges);
+        Polygon_2::Segment_2 edge = visibleEdges[index].edge;
+        std::cout << "Chosen Edge: " << edge << std::endl;
+        
+        
         // Insert the new point to A inbetween the chosen edges' source and target
-        int index = findPolygonPoint(A, edge.target()); 
+        index = findPolygonPoint(A, edge.target()); 
         A.insert(A.begin() + index, polygon[i]);
         std::cout << "------------ A ------------" << std::endl;
         printPolygon(A);
+        const bool simpl = A.is_simple();
+        if (simpl)
+            std::cout << "Simple polygon" << std::endl;
+        else
+        {
+            std::cout << "Not simple" << std::endl;
+        }
         std::cout << "---------------------------" << std::endl;
         continue;
 
@@ -274,20 +301,20 @@ int main(int argc, char** argv)
         convexHull.push_back(polygon[i]);
 
 
-        const bool simpl = convexHull.is_simple();
-        if (simpl)
-            std::cout << "Simple polygon" << std::endl;
-        else
-        {
-            std::cout << "Not simple" << std::endl;
-            //Swap last and second to last points
-            std::swap(convexHull[convexHull.size() - 1], convexHull[convexHull.size() - 2]);
-            const bool simpl = convexHull.is_simple();
-            if (simpl)
-                std::cout << "Made it a Simple polygon" << std::endl;
-            else
-                std::cout << "Still not simple" << std::endl;
-        }
+        // const bool simpl = convexHull.is_simple();
+        // if (simpl)
+        //     std::cout << "Simple polygon" << std::endl;
+        // else
+        // {
+        //     std::cout << "Not simple" << std::endl;
+        //     //Swap last and second to last points
+        //     std::swap(convexHull[convexHull.size() - 1], convexHull[convexHull.size() - 2]);
+        //     const bool simpl = convexHull.is_simple();
+        //     if (simpl)
+        //         std::cout << "Made it a Simple polygon" << std::endl;
+        //     else
+        //         std::cout << "Still not simple" << std::endl;
+        // }
 
 
         printPolygon(convexHull);
@@ -357,17 +384,18 @@ void vertexColoring(std::vector<ColoredVertex>& coloredVertices, int color, Poin
     }
 }
 
-void findVisibleEdges(Polygon_2 polygon, Point_2 point, ColoredEdge redEdge, std::vector<Polygon_2::Segment_2>& visibleEdges)
+void findVisibleEdges(Polygon_2 polygon, Point_2 point, ColoredEdge redEdge, std::vector<EdgeAreaTuple>& visibleEdges)
 {
     // Find index of redEdge.source() in polygon  
     int indexSource = findPolygonPoint(polygon, redEdge.edge.source());
     int indexTarget = findPolygonPoint(polygon, redEdge.edge.target());
     // If edge is on polygon
-    if(indexSource == indexTarget -1 || (indexSource == polygon.size() - 1 && indexTarget == 0))
+    if(indexSource == indexTarget -1 || (indexSource == polygon.size() - 1 && indexTarget == 0)
+     ||indexTarget == indexSource -1 || (indexTarget == polygon.size() - 1 && indexSource == 0))
     {
         // The edge is visible
-        visibleEdges.push_back(redEdge.edge);
-
+        Triangle_2 triangle(polygon[indexSource], polygon[indexTarget], point);        
+        visibleEdges.push_back(EdgeAreaTuple(redEdge.edge, std::abs(triangle.area())));
     }
     else
     {
@@ -376,32 +404,78 @@ void findVisibleEdges(Polygon_2 polygon, Point_2 point, ColoredEdge redEdge, std
         {
             Triangle_2 triangle(polygon[i], polygon[(i + 1) % polygon.size()], point);
 
-            // Get the firdt edge of the triangle
-            Polygon_2::Segment_2 edge = polygon.edge(i);
+            Polygon_2::Segment_2 candidateEdge(polygon[i], polygon[(i + 1) % polygon.size()]);
 
+            // CHECK IF THE POLYGON INTERSECTS THE TRIANGLE
+            bool isIntersecting = false;
+            
 
-            const auto mutual = CGAL::intersection(triangle, edge);
-            if (mutual)
+            for( int j = 0; j < polygon.size(); j++)
             {
-                // If the intersection is a Segment
-                if (const Polygon_2::Segment_2* mutualSegment = boost::get<Polygon_2::Segment_2>(&*mutual))
+                Polygon_2::Segment_2 edge = polygon.edge(j);
+
+                const auto mutual = CGAL::intersection(triangle, edge);
+                if (mutual)
                 {
-                    std::cout << *mutualSegment << std::endl;
-                    // Check if the mutualSegment is the same as the edge and add it to the visible edges
-                    if (mutualSegment->source() == edge.source() && mutualSegment->target() == edge.target())
+                    // If the intersection is a Segment
+                    if (const Polygon_2::Segment_2* mutualSegment = boost::get<Polygon_2::Segment_2>(&*mutual))
                     {
-                        // The edge is visible
-                        visibleEdges.push_back(edge);
+                        // Check if the mutualSegment is the same as the edge and ignore it
+                        if (mutualSegment->source() == edge.source() && mutualSegment->target() == edge.target())
+                            continue;
+                        else
+                        {
+                            // Found an intersection Segment, therefore the edge in question,
+                            // polygon[i] to polygon[(i + 1) % polygon.size()],  is not visible
+                            // std::cout << "Intersecting Segment" << std::endl;
+                            isIntersecting = true;
+                            break;
+                        }
+
+                    }
+                    else
+                    // If the intersection is a point then the polygon and the triangle
+                    // share a vertex, therefore the edge in question can still be visible
+                    {
+                        const Point_2* mutualPoint = boost::get<Point_2 >(&*mutual);
+                        // std::cout << "Intersecting Point" << std::endl;
                     }
                 }
-                else
-                // If the intersection is a point then it's not visible 
-                {
-                    const Point_2* mutualPoint = boost::get<Point_2 >(&*mutual);
-                    std::cout << *mutualPoint << std::endl;
-                }
             }
+            if (!isIntersecting)
+                // The edge is visible
+                visibleEdges.push_back(EdgeAreaTuple(candidateEdge, std::abs(triangle.area())));
         }
 
     }
+}
+
+int findMinAreaEdge(std::vector<EdgeAreaTuple> visibleEdges)
+{
+    int index = 0;
+    double minArea = visibleEdges[0].area;
+    for (int i = 1; i < visibleEdges.size(); i++)
+    {
+        if (visibleEdges[i].area < minArea)
+        {
+            minArea = visibleEdges[i].area;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int findMaxAreaEdge(std::vector<EdgeAreaTuple> visibleEdges)
+{
+    int index = 0;
+    double maxArea = visibleEdges[0].area;
+    for (int i = 1; i < visibleEdges.size(); i++)
+    {
+        if (visibleEdges[i].area > maxArea)
+        {
+            maxArea = visibleEdges[i].area;
+            index = i;
+        }
+    }
+    return index;
 }
